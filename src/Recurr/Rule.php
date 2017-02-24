0fb49a33c8da46d83d4b6efa702c7f700814634b
fentie <brian.fenton@wheniwork.com>
2017-02-24 15:37:43 -0600

    strlen will always return an int so === is safe

--- src/Recurr/Rule.php
+++ src/Recurr/Rule.php
@@ -932,7 +932,7 @@ class Rule
         }
 
         foreach ($byDay as $idx => $day) {
-            if (strlen($day) == 2) {
+            if (strlen($day) === 2) {
                 $byDay[$idx] = new Weekday($day, null);
             } else {
                 preg_match('/^([+-]?[0-9]+)([A-Z]{2})$/', $day, $dayParts);
