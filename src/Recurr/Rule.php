06a90b9c1b7aa91de0351a52485e850a00ee7c51
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-06-08 11:21:53 +0100

    parse TZID in rrule string

--- src/Recurr/Rule.php
+++ src/Recurr/Rule.php
@@ -210,7 +210,7 @@ class Rule
 
         if (is_array($rrule)) {
             $this->loadFromArray($rrule);
-        } else if (!empty($rrule)) {
+        } elseif (!empty($rrule)) {
             $this->loadFromString($rrule);
         }
     }
@@ -286,35 +286,66 @@ class Rule
      */
     public function parseString($rrule)
     {
-        if (strpos($rrule, 'DTSTART:') === 0) {
-            $pieces = explode(':', $rrule);
+        //rrule here can be:
+        //1) DTSTART:20200607T120200
+        //2) DTSTART;TZID=UTC:20200607T120200
+        //3) RRULE:FREQ=DAILY;INTERVAL=1
+        //4) FREQ=DAILY;DTSTART;TZID=UTC:20200607T120200;INTERVAL=1
+        //5) FREQ=DAILY;DTSTART:20200607T120200;INTERVAL=1
 
-            if (count($pieces) !== 2) {
-                throw new InvalidRRule('DSTART is not valid');
-            }
 
-            return array('DTSTART' => $pieces[1]);
-        }
+        //SOLUTION
+        //split by ';':
+        //1) [DTSTART:20200607T120200]
+        //2) [DTSTART, TZID=UTC:20200607T120200]
+        //3) [RRULE:FREQ=DAILY, INTERVAL=1]
+        //4) [FREQ=DAILY, DTSTART, TZID=UTC:20200607T120200, INTERVAL=1]
+        //5) [FREQ=DAILY, DTSTART:20200607T120200, INTERVAL=1]
 
-        if (strpos($rrule, 'RRULE:') === 0) {
-            $rrule = str_replace('RRULE:', '', $rrule);
-        }
+        $fragments = explode(';', $rrule);
 
-        $pieces = explode(';', $rrule);
-        $parts  = array();
-
-        if (!count($pieces)) {
+        if (!count($fragments)) {
             throw new InvalidRRule('RRULE is empty');
         }
 
-        // Split each piece of the RRULE in to KEY=>VAL
-        foreach ($pieces as $piece) {
-            if (false === strpos($piece, '=')) {
+        $parts  = array();
+        foreach ($fragments as $fragment) {
+            if (strpos($fragment, 'RRULE:') === 0) {
+                $fragment = str_replace('RRULE:', '', $fragment);
+            }
+
+
+            if (strpos($fragment, 'DTSTART') === 0) {
+                if ($fragment === 'DTSTART') {
+                    $parts['DTSTART'] = '';//to be replaced by next token
+
+                    continue;
+                }
+
+                $p = explode(':', $fragment);
+                if (count($p) !== 2) {
+                    throw new InvalidRRule('DTSTART is not valid');
+                }
+
+                $parts['DTSTART'] = $p[1];
+
                 continue;
             }
 
-            list($key, $val) = explode('=', $piece);
-            $parts[$key] = $val;
+            if (strpos($fragment, '=')) {
+                list($key, $val) = explode('=', $fragment);
+
+                if ($key === 'TZID') {
+                    $p = explode(':', $val);
+
+                    $parts['TZID'] = $p[0];
+                    $parts['DTSTART'] = $p[1];
+
+                    continue;
+                }
+
+                $parts[$key] = $val;
+            }
         }
 
         return $parts;
@@ -341,11 +372,26 @@ class Rule
             $this->setFreq(self::$freqs[$parts['FREQ']]);
         }
 
+        // TZID
+        if (isset($parts['TZID'])) {
+            $this->setTimezone($parts['TZID']);
+        }
+
         // DTSTART
         if (isset($parts['DTSTART'])) {
             $this->isStartDateFromDtstart = true;
-            $date = new \DateTime($parts['DTSTART']);
-            $date = $date->setTimezone(new \DateTimeZone($this->getTimezone()));
+
+            $timezone = new \DateTimeZone($this->getTimezone());
+            $date = null;
+            if (isset($parts['TZID'])) {
+                //DTSTART is datetime in TZID timezone
+                $date = new \DateTime($parts['DTSTART'], $timezone);
+            } else {
+                //DTSTART is UTC, convert to timezone coming from constructor/startDate/default
+                $date = new \DateTime($parts['DTSTART']);
+                $date = $date->setTimezone($timezone);
+            }
+
             $this->setStartDate($date);
         }
 
